6be1fb8b302faac0773b70afe6e5a088b2845fbe
Muhammad Noman <muhammad.noman@umlaut.com>
2024-03-25 09:49:31 +0100

    fix for internet issues failure

--- selenium_test.py
+++ selenium_test.py
@@ -1,26 +1,27 @@
 import datetime
 import time
 
-from selenium.common import exceptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
+
 def Check_appointment():
     option = webdriver.ChromeOptions()
-    # option.add_argument("start-minimized")
-    # option.add_argument('--headless')
-    # option.add_argument('--disable-gpu')
+    # option.add_argument("start-maximized")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
 
     today = datetime.datetime.now()
 
     driver.get('https://termine.staedteregion-aachen.de/auslaenderamt/?rs')
+    time.sleep(2)
     driver.find_element(By.ID, "buttonfunktionseinheit-1").click()
+    time.sleep(2)
 
     urls = [
         "https://termine.staedteregion-aachen.de/auslaenderamt/location?mdt=78&select_cnc=1&cnc-204=0&cnc-205=0&cnc-198=1&cnc-201=0&cnc-202=0&cnc-227=0&cnc-232=0&cnc-203=0&cnc-196=0&cnc-190=0&cnc-185=0&cnc-187=0&cnc-188=0&cnc-186=0&cnc-192=0&cnc-191=0&cnc-194=0&cnc-197=0&cnc-193=0&cnc-183=0&cnc-184=0&cnc-195=0&cnc-200=0&cnc-228=0",
@@ -30,30 +31,36 @@ def Check_appointment():
 
     for i in range(len(urls)):
         driver.get(urls[i])
-        retries = 20
-        msg = f"{today}: No appointment available in url: {i + 1}"
-
-
-        # Tries to find the button for number of retries and breaks the loop once the element is clickable
-        for j in range(retries):
+        time.sleep(3)
+        ActionChains(driver).scroll_by_amount(0, 500).perform()
+        while 1:
             try:
                 driver.find_element(By.CSS_SELECTOR,
                             "input[type='submit'][aria-label='Ausländeramt Aachen, 2. Etage auswählen'][name='select_location'][value='Ausländeramt Aachen, 2. Etage auswählen']").click()
                 break
-            except exceptions.ElementClickInterceptedException as e:
-                time.sleep(1)
+            except Exception as e:
+                time.sleep(5)
 
-        try:
-            if "https://termine.staedteregion-aachen.de/auslaenderamt/suggest" in driver.current_url:
+        trial = 1
+        found = 1
+        while trial < 6:
+            try:
                 driver.find_element(By.XPATH, "//*[contains(text(), 'Kein freier Termin verfügbar')]")
-            else:
-                msg = f"{today}: For url: {i+1} , not able to get the click button because of internet issue. Increase the retries limit."
-        except Exception as e:
+                found = 0
+                break
+            except Exception as e:
+                time.sleep(5)
+                trial += 1
+        if found == 0:
+            msg = f"{today}: No appointment available in url: {i + 1}"
+        else:
             msg = f"{today}: -------------------   Appointment found in url: {i + 1} ----------------"
-            elem = driver.find_element(By.XPATH, "//*")
-            source_code = elem.get_attribute("outerHTML")
-            with open('final_page.html', "w") as file:
-                file.write(source_code.encode('utf-8'))
+            # driver.save_screenshot(f"{today}.{i}.png");
+            with open(f"{today}.{i}.html", "w", encoding='utf-8') as f:
+                f.write(driver.page_source)
+            for x in range(20):
+                print('\a')
+                time.sleep(1)
 
         print(msg)
 
@@ -65,5 +72,5 @@ def Check_appointment():
 
 Check_appointment()
 scheduler = BlockingScheduler()
-scheduler.add_job(Check_appointment, 'interval', hours=0.1)
+scheduler.add_job(Check_appointment, 'interval', minutes=5)
 scheduler.start()
